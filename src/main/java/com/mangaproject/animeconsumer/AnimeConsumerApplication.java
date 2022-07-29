7aa3b858346e864140bd233157da38088cbd28dd
Patricia Tami Sakita <67209629+tamisakita@users.noreply.github.com>
2022-07-29 12:56:02 -0300

    Initial commit

---
+++ src/main/java/com/mangaproject/animeconsumer/AnimeConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.mangaproject.animeconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangaproject/animeconsumer/AnimeConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangaproject.animeconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
