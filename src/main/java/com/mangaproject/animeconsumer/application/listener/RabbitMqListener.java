badac92ea3705ab678bd5d671922640231c5a276
Patricia Tami Sakita <67209629+tamisakita@users.noreply.github.com>
2022-08-05 15:51:45 -0300

    refatorando codigo

--- src/main/java/com/mangaproject/animeconsumer/application/listener/RabbitMqListener.java
+++ src/main/java/com/mangaproject/animeconsumer/application/listener/RabbitMqListener.java
@@ -4,24 +4,33 @@ import com.mangaproject.animeconsumer.application.presentation.representation.An
 import com.mangaproject.animeconsumer.domain.utils.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Component
 @Slf4j
 public class RabbitMqListener {
 
-    @RabbitListener(queues = {"${queue.name}"})
+    private final RabbitTemplate rabbitTemplate;
+
+    private static final String ANIME_RETURN_MESSAGE = "Message received!";
+    private static final String ANIME_EXCHANGE = "animes";
+
+    public RabbitMqListener(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @RabbitListener(queues = {"anime-project"})
     public void receiveSavedAnime(@Payload String message) {
         try {
-            System.out.println("The anime has been saved " + message);
+            AnimeResponseRepresentation anime = JsonUtil.fromJson(message, AnimeResponseRepresentation.class);
+            System.out.println("The anime has been saved " + anime);
+            rabbitTemplate.convertAndSend(ANIME_EXCHANGE, "anime-out-key", ANIME_RETURN_MESSAGE);
         } catch (Throwable e) {
-            log.error("Deu erro", e);
+            log.error("Could not save the anime", e);
         }
     }
 
@@ -31,8 +40,9 @@ public class RabbitMqListener {
             AnimeResponseRepresentation[] animes = JsonUtil.fromJson(message, AnimeResponseRepresentation[].class);
             var animeList = Arrays.asList(animes);
             System.out.println("All animes has been received " + animeList);
+            rabbitTemplate.convertAndSend(ANIME_EXCHANGE, "anime-out-key", ANIME_RETURN_MESSAGE);
         } catch (Throwable e) {
-            log.error("Deu erro", e);
+            log.error("Could not find the list of animes", e);
         }
     }
 }
